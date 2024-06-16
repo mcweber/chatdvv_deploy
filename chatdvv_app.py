7e372ccc9ccc7bc8305a46264c1b404c87e1262f
Martin <webermac@proton.me>
2024-06-16 10:05:56 +0200

    Bug fixes. Vector search updated. Filter updated. UI updated.

--- chatdvv_app.py
+++ chatdvv_app.py
@@ -1,14 +1,16 @@
 # ---------------------------------------------------
-# Version: 09.06.2024
+# Version: 15.06.2024
 # Author: M. Weber
 # ---------------------------------------------------
 # 05.06.2024 added searchFilter in st.session_state and sidebar
 # 07.06.2024 implemented rag with fulltext search
 # 09.06.2024 activated user management. Statistiken implemented.
+# 15.06.2024 added filter for textsearch
 # ---------------------------------------------------
 
 import os
 import streamlit as st
+import pandas as pd
 import chatdvv_module as myapi
 import user_management
 
@@ -18,13 +20,10 @@ SEARCH_TYPES = {"vector": "Vector search", "llm": "LLM search", "rag": "RAG sear
 
 @st.experimental_dialog("Login User")
 def login_user_dialog() -> None:
-
     with st.form(key="loginForm"):
-
         st.write(f"Status: {st.session_state.userStatus}")
         user_name = st.text_input("Benutzer")
         user_pw = st.text_input("Passwort", type="password")
-
         if st.form_submit_button("Login"):
             if user_name and user_pw:
                 active_user = user_management.check_user(user_name, user_pw)
@@ -37,9 +36,9 @@ def login_user_dialog() -> None:
             else:
                 st.error("Please fill in all fields.")
 
+
 @st.experimental_dialog("Statistiken")
 def statistiken_dialog() -> None:
-
     st.write(f"Anzahl Artikel: {myapi.collection.count_documents({})}")
     st.write(f"Anzahl Artikel ohne Abstract: {myapi.collection.count_documents({'ki_abstract': ''})}")
     st.write(f"Anzahl Artikel ohne Embeddings: {myapi.collection.count_documents({'embeddings': {}})}")
@@ -53,7 +52,6 @@ def statistiken_dialog() -> None:
 # Main -----------------------------------------------------------------
 
 def main() -> None:
-    
     st.set_page_config(page_title='DVV Insight', initial_sidebar_state="collapsed")
     
     # Initialize Session State -----------------------------------------
@@ -70,16 +68,14 @@ def main() -> None:
         st.session_state.results = ""
         st.session_state.history = []
         st.session_state.searchType = "rag" # llm, vector, rag, fulltext
-
     if st.session_state.userStatus == False:
         login_user_dialog()
         # st.experimental_rerun()
-
     st.header("DVV Insight")
     # st.subheader("Research Bot für Fachartikel im Bereich Transport und Verkehr")
     col1, col2 = st.columns(2)
     with col1:
-        st.write("Version 0.1.3 - 09.06.2024")
+        st.write("Version 0.1.4 - 15.06.2024")
     with col2:
         if st.session_state.userStatus:
             st.write(f"Eingeloggt als: {st.session_state.userName}")
@@ -88,87 +84,85 @@ def main() -> None:
 
     # Define Sidebar ---------------------------------------------------
     with st.sidebar:
-
         switch_searchFilter = st.multiselect(label="Choose Publications", options=st.session_state.feldListe, default=st.session_state.searchFilter)
         if switch_searchFilter != st.session_state.searchFilter:
             st.session_state.searchFilter = switch_searchFilter
             st.experimental_rerun()
-        
-        switch_searchType = st.radio(label="Choose Search Type", options=("rag", "llm", "vector", "fulltext"), index=0)
-        if switch_searchType != st.session_state.searchType:
-            st.session_state.searchType = switch_searchType
+        if st.button("Reset Filter"):
+            st.session_state.searchFilter = st.session_state.feldListe
             st.experimental_rerun()
-
+        # switch_searchType = st.radio(label="Choose Search Type", options=("rag", "llm", "vector", "fulltext"), index=0)
+        # if switch_searchType != st.session_state.searchType:
+        #     st.session_state.searchType = switch_searchType
+        #     st.experimental_rerun()
         switch_search_results = st.slider("Search Results", 1, 20, st.session_state.searchResults)
         if switch_search_results != st.session_state.searchResults:
             st.session_state.searchResults = switch_search_results
             st.experimental_rerun()
-
         switch_llm = st.radio(label="Switch to LLM", options=("groq", "openai"), index=1)
         if switch_llm != st.session_state.llmStatus:
             st.session_state.llmStatus = switch_llm
             st.experimental_rerun()
-
         switch_SystemPrompt = st.text_area("System-Prompt", st.session_state.systemPrompt)
         if switch_SystemPrompt != st.session_state.systemPrompt:
             st.session_state.systemPrompt = switch_SystemPrompt
             st.experimental_rerun()
-
         if st.button("Statistiken"):
             statistiken_dialog()
-            
         if st.button("Logout"):
             st.session_state.userStatus = False
             st.session_state.searchStatus = False
             st.session_state.userName = ""
             st.experimental_rerun()
 
+    # Define Search Type ------------------------------------------------
+    switch_searchType = st.radio(label="Choose Search Type", options=("rag", "llm", "vector", "fulltext"), index=0, horizontal=True)
+    if switch_searchType != st.session_state.searchType:
+        st.session_state.searchType = switch_searchType
+        st.experimental_rerun()
+
     # Define Search Form ----------------------------------------------
     with st.form(key="searchForm"):
         question = st.text_input(SEARCH_TYPES[st.session_state.searchType])
-
         if st.session_state.searchType in ["rag", "llm"]:
             button_caption = "Fragen"
         else:
             button_caption = "Suchen"
-        
         if st.form_submit_button(button_caption) and question != "":
             st.session_state.searchStatus = True
         
     # Define Search & Search Results -------------------------------------------
     if st.session_state.userStatus and st.session_state.searchStatus:
-
         if st.session_state.searchType == "fulltext":
-
-            results, results_count = myapi.text_search(search_text=question, limit=st.session_state.searchResults)
-
+            results, results_count = myapi.text_search(
+                search_text=question, 
+                filter=st.session_state.searchFilter, 
+                limit=st.session_state.searchResults
+                )
             # st.caption(f'Suche nach: "{question}". {results_count} Artikel gefunden.')
-            st.session_state.searchStatus = False
-
-            counter = 1
+            df = pd.DataFrame(results)
+            output = df[["quelle_id", "nummer", "jahrgang", "titel", "text"]]
+            st.dataframe(output)
+            # counter = 1
+            # for result in results:
+            #     # st.write(f"[{result['datum']}] {result['titel']}")
+            #     st.write(f"[{result['quelle_id']}, {result['nummer']}/{result['jahrgang']}] {result['titel']}")
+            #     st.write(result['text'][:500] + " ...")
+            #     st.divider()
+            #     counter += 1
+            #     if counter > st.session_state.searchResults:
+            #         break
+        elif st.session_state.searchType == "vector":
+            results = myapi.vector_search(
+                query_string=question, 
+                limit=st.session_state.searchResults
+                )
             for result in results:
                 # st.write(f"[{result['datum']}] {result['titel']}")
                 st.write(f"[{result['quelle_id']}, {result['nummer']}/{result['jahrgang']}] {result['titel']}")
-                st.write(result['text'][:500] + " ...")
+                st.write(result['text'])
                 st.divider()
-                counter += 1
-                if counter > st.session_state.searchResults:
-                    break
-
-        elif st.session_state.searchType == "vector":
-
-                    results = myapi.vector_search(query_string=question, limit=st.session_state.searchResults)
-
-                    for result in results:
-                        # st.write(f"[{result['datum']}] {result['titel']}")
-                        st.write(f"[{result['quelle_id']}, {result['nummer']}/{result['jahrgang']}] {result['titel']}")
-                        st.write(result['text'])
-                        st.divider()
-
-                    st.session_state.searchStatus = False
-
         elif st.session_state.searchType == "llm":
-
             summary = myapi.ask_llm(
                 llm=st.session_state.llmStatus,
                 temperature=0.2,
@@ -177,21 +171,19 @@ def main() -> None:
                 systemPrompt=st.session_state.systemPrompt,
                 results_str=""
                 )
-
             st.write(summary)
-            st.session_state.searchStatus = False
-
         elif st.session_state.searchType == "rag":
-
             # results = myapi.vector_search(question, st.session_state.searchResults)
-            results, results_count = myapi.text_search(search_text=question, limit=st.session_state.searchResults)
-
+            results, results_count = myapi.text_search(
+                search_text=question, 
+                filter=st.session_state.searchFilter, 
+                limit=st.session_state.searchResults
+                )
             with st.expander("DB Suchergebnisse"):
                 results_str = ""
                 for result in results:
                     st.write(f"[{result['quelle_id']}, {result['nummer']}/{result['jahrgang']}] {result['titel']}")
                     results_str += f"Datum: {result['datum']}\nTitel: {result['titel']}\nText: {result['text']}\n\n"
-
             summary = myapi.ask_llm(
                 llm=st.session_state.llmStatus,
                 temperature=0.2,
@@ -200,9 +192,9 @@ def main() -> None:
                 systemPrompt=st.session_state.systemPrompt,
                 results_str=results_str
                 )
-
             st.write(summary)
-            st.session_state.searchStatus = False
+        st.session_state.searchStatus = False
+
 
 if __name__ == "__main__":
     main()
--- chatdvv_module.py
+++ chatdvv_module.py
@@ -1,8 +1,12 @@
 # ---------------------------------------------------
-# Version: 09.06.2024
+# Version: 15.06.2024
 # Author: M. Weber
 # ---------------------------------------------------
 # 07.06.2024 Adapted fulltext search to atlas search
+# 13.06.2024 Added get_document()
+# 15.06.2024 Added reset filter. Added filter for textsearch
+# 15.06.2024 Added torch model for embeddings
+# 15.06.2024 Updated vector_search to use text_embeddings
 # ---------------------------------------------------
 
 from datetime import datetime
@@ -16,47 +20,44 @@ import openai
 from groq import Groq
 import ollama
 
+import torch
+from transformers import AutoTokenizer, AutoModel
+
 # Init MongoDB Client
 load_dotenv()
 mongoClient = MongoClient(os.environ.get('MONGO_URI_DVV'))
 database = mongoClient.dvv_content_pool
 collection = database.dvv_artikel
-
 openaiClient = openai.OpenAI(api_key=os.environ.get('OPENAI_API_KEY_DVV'))
-
 groqClient = Groq(api_key=os.environ['GROQ_API_KEY_PRIVAT'])
+
+# Load pre-trained model and tokenizer
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
 
 # Define Database functions ----------------------------------
 
 def generate_abstracts(input_field: str, output_field: str, max_iterations: int = 20) -> None:
-
     cursor = collection.find({output_field: ""}).limit(max_iterations)
-
     iteration = 0
-
     for record in cursor:
-
         iteration += 1
-
         start_time = datetime.now()
         abstract = write_summary(str(record[input_field]))
         end_time = datetime.now()
         duration = end_time - start_time
-
         print(record['titel'][:50])
         print(f"#{iteration} Duration: {round(duration.total_seconds(), 2)}")
         print("-"*50)
-
         collection.update_one({"_id": record.get('_id')}, {"$set": {output_field: abstract}})
-
     cursor.close()
 
 
 def write_summary(text: str) -> str:
-
     if text == "":
         return "empty"
-
     systemPrompt = """
                     Du bist ein Redakteur im Bereich Transport und Verkehr.
                     Du bis Experte dafür, Zusammenfassungen von Fachartikeln zu schreiben.
@@ -69,61 +70,57 @@ def write_summary(text: str) -> str:
             Verwende keine Zeilenumrüche oder Absätze.
             Die Antwort darf nur aus dem eigentlichen Text der Zusammenfassung bestehen.
             """
-   
     prompt = [
             {"role": "system", "content": systemPrompt},
             {"role": "assistant", "content": f'Originaltext: {text}'},
             {"role": "user", "content": task}
             ]
-    
     response = openaiClient.chat.completions.create(
             model="gpt-4o",
             temperature=0.1,
             messages = prompt
             )
-    
     return response.choices[0].message.content
 
 
 def generate_embeddings(input_field: str, output_field: str, 
                         max_iterations: int = 10) -> None:
-
     query = {output_field: {}}
     cursor = collection.find(query)
-    count = collection.count_documents(query)
-    print(f"Records without embeddings: {count}")
-
+    # count = collection.count_documents(query)
+    # print(f"Records without embeddings: {count}")
     iteration = 0
-
     for record in cursor:
-
         iteration += 1
         if iteration > max_iterations:
             break
-
         article_text = record[input_field]
         if article_text == "":
             article_text = "Fehler: Kein Text vorhanden."
         else:
             embeddings = create_embeddings(article_text)
             collection.update_one({"_id": record['_id']}, {"$set": {output_field: embeddings}})
-
     print(f"\nGenerated embeddings for {iteration} records.")
 
 
-def create_embeddings(text: str) -> str:
+# def create_embeddings(text: str) -> str:
+#     if text == "":
+#         return "empty"
+#     model = "text-embedding-3-small"
+#     text = text.replace("\n", " ")
+#     return openaiClient.embeddings.create(input = [text], model=model).data[0].embedding
 
-    if text == "":
-        return "empty"
-    
-    model = "text-embedding-3-small"
-    text = text.replace("\n", " ")
-    return openaiClient.embeddings.create(input = [text], model=model).data[0].embedding
+
+def create_embeddings(text: str) -> list:
+    inputs = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
+    embeddings_list = outputs.last_hidden_state.mean(dim=1).squeeze().tolist()
+    return embeddings_list
 
 
 def ask_llm(llm: str, temperature: float = 0.2, question: str = "", history: list = [],
             systemPrompt: str = "", results_str: str = "") -> str:
-
     # define prompt
     input_messages = [
                 {"role": "system", "content": systemPrompt},
@@ -131,7 +128,6 @@ def ask_llm(llm: str, temperature: float = 0.2, question: str = "", history: lis
                 {"role": "assistant", "content": 'Hier sind einige relevante Informationen:\n'  + results_str},
                 {"role": "user", "content": 'Basierend auf den oben genannten Informationen, ' + question}
                 ]
-
     if llm == "openai":
         response = openaiClient.chat.completions.create(
             model="gpt-4o",
@@ -139,7 +135,6 @@ def ask_llm(llm: str, temperature: float = 0.2, question: str = "", history: lis
             messages = input_messages
             )
         output = response.choices[0].message.content
-
     elif llm == "groq":
         response = groqClient.chat.completions.create(
             model="mixtral-8x7b-32768",
@@ -147,23 +142,25 @@ def ask_llm(llm: str, temperature: float = 0.2, question: str = "", history: lis
             messages=input_messages
         )
         output = response.choices[0].message.content
-
     elif llm == "ollama_mistral":
         response = ollama.chat(model="mistral", messages=input_messages)
         output = response['message']['content']
-
     elif llm == "ollama_llama3":
         response = ollama.chat(model="llama3", messages=input_messages)
         output = response['message']['content']
-
     else:
         output = "Error: No valid LLM specified."
-
     return output
 
 
-def text_search(search_text : str = "*", limit : int = 10) -> [tuple, int]:
+def generate_filter(filter: list, field: str) -> dict:
+    if filter:
+        return {field: {"$in": filter}}
+    else:
+        return {}
+    
 
+def text_search(search_text : str = "*", filter : list = [], limit : int = 10) -> [tuple, int]:
     # query = {"$search": {"index": "volltext", "text": search_text, "path": {"wildcard": "*"}}}
     query = {
         "index": "volltext",
@@ -173,7 +170,6 @@ def text_search(search_text : str = "*", limit : int = 10) -> [tuple, int]:
             "path": {"wildcard": "*"}
             }
         }
-    
     fields = {
         "_id": 1,
         "quelle_id": 1,
@@ -186,43 +182,28 @@ def text_search(search_text : str = "*", limit : int = 10) -> [tuple, int]:
         "text": 1, 
         "ki_abstract": 1
         }
-
     pipeline = [
         {"$search": query},
+        {"$match": generate_filter(filter, "quelle_id")},
         {"$project": fields},
         {"$limit": limit}
         ]
-
-    # pipeline_meta = [
-    #     {
-    #         "$searchMeta": query,
-    #         "$count": {
-    #             "type": "total"
-    #             }
-    #         }
-    #     ]
-
     cursor = collection.aggregate(pipeline)
     # count = collection.aggregate(pipeline_meta)
     count = 0
-
     return cursor, count
 
 
-def vector_search(query_string: str = "", limit: int = 10) -> tuple:
-
+def vector_search(query_string: str = "", filter : list = [], sort: str = "date", limit: int = 10) -> tuple:
     embeddings_query = create_embeddings(query_string)
-
-    pipeline = [
-        {"$vectorSearch": {
-            "index": "nahv_vector_index",
-            "path": "embeddings",
+    query = {
+            "index": "text_vector_index",
+            "path": "text_embeddings",
             "queryVector": embeddings_query,
             "numCandidates": int(limit * 10),
             "limit": limit,
             }
-        },
-        {"$project": {
+    fields = {
             "_id": 1,
             "quelle_id": 1,
             "jahrgang": 1,
@@ -232,28 +213,27 @@ def vector_search(query_string: str = "", limit: int = 10) -> tuple:
             "untertitel": 1,
             "text": 1,
             "ki_abstract": 1,
+            "date": 1,
             "score": {"$meta": "vectorSearchScore"}
             }
-        }
+    pipeline = [
+        {"$vectorSearch": query},
+        {"$match": generate_filter(filter, "quelle_id")},
+        {"$sort": {sort: -1}},
+        {"$project": fields}
         ]
-
-    result = collection.aggregate(pipeline)
-
-    return result
+    return collection.aggregate(pipeline)
 
 
 def print_results(cursor: list) -> None:
-
     if not cursor:
         print("Keine Artikel gefunden.")
-
     for item in cursor:
         print(f"[{str(item['datum'])[:10]}] {item['titel'][:70]}")
         # print("-"*80)
 
 
 def group_by_field() -> dict:
-
     pipeline = [
             {
             '$group': {
@@ -269,15 +249,18 @@ def group_by_field() -> dict:
             }
             ]
     result = collection.aggregate(pipeline)
-    
     # transfor into dict
     return_dict = {}
     for item in result:
         return_dict[item['_id']] = item['count']
-       
     return return_dict
 
 
 def list_fields() -> dict:
     result = collection.find_one()
     return result.keys()
+
+
+def get_document(id: str) -> dict:
+    document = collection.find_one({"id": id})
+    return document
